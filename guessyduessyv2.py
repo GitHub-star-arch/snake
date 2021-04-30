bb3ebf767f793cef88fcea80817136d3f5b2008b
GitHub-star-arch <63858220+GitHub-star-arch@users.noreply.github.com>
2021-04-30 14:22:58 +0530

    Add files via upload

---
+++ guessyduessyv2.py
@@ -0,0 +1,18 @@
+import random
+
+number=int(random.randint(1,100))
+chance=int(3)
+gc=bool(False)
+while (0!=chance and gc==bool(False)):
+    guess1=int(input("You have "+ str(chance) +" lives. Guess a number between 1-100:"))
+    if (guess1==number):
+        gc=bool(True)
+        print("You are great")
+    elif (guess1>number):
+        print("Try lower.")
+    elif (guess1<number):
+        print("Try Higher")
+    chance=chance-1
+if (gc!=bool(True)):
+    print("Is that trash I smell oh wait its you.")
+input('press enter...')
